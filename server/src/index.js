2d6ed91d89907a22fc8a529e467ee74bab45fcc8
vermathe1 <verma.the1@gmail.com>
2020-05-08 13:20:08 +0530

    add nodemon

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 9000;
 
-app.get('/',(req,res)=>res.send("hello world!!"));
+app.get('/',(req,res)=>res.send("hello world!!!!"));
 app.listen(port);
 console.log(`[app]: http://localhost:${port}`);
