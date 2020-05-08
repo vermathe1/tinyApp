378af90f49f3c45dfd9076b9cd4e88955c24bee6
vermathe1 <verma.the1@gmail.com>
2020-05-08 13:01:33 +0530

    express server setup

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 9000;
+
+app.get('/',(req,res)=>res.send("hello world!!"));
+app.listen(port);
+console.log(`[app]: http://localhost:${port}`);
